0bdd0f6527d4800347094cc0d14e1554eccbb1eb
devkazimhaider <ahsanraza00059@gmail.com>
2025-01-02 02:04:56 +0500

    (setup): db connection

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-
+import dbConnection from './dbConfig/index.js';
 // security packages
 import helmet from 'helmet';
 
@@ -13,14 +13,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8800;
 
+// Connect to MongoDB
+dbConnection();
+
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({extended: true}));
-
-// morgan middleware for logging requests
 app.use(morgan('dev'));
 
 app.listen(PORT, ()=>{
